05085ea88edc5f0c2d0b3495a19c96599397842d
Alexander Shampton <120408186+alexandershampton@users.noreply.github.com>
2024-01-06 11:11:20 -0500

    Delete CS1181_Project3 directory

--- CS1181_Project3/ExpressCheckoutLine.java
+++
@@ -1,6 +0,0 @@
-public class ExpressCheckoutLine extends CheckoutLine{
-    ExpressCheckoutLine(int lineNumber, double simClock,int lineSize,double waitTime){
-        super(lineNumber, simClock,lineSize,waitTime);
-    }
-
-}
--- CS1181_Project3/RegularCheckoutLine.java
+++
@@ -1,5 +0,0 @@
-public class RegularCheckoutLine extends CheckoutLine{
-    RegularCheckoutLine(int lineNumber, double simClock,int lineSize,double waitTime){
-        super(lineNumber, simClock,lineSize,waitTime);
-    }
-}
